1ca876e1fc848e99ae582b17dd9f06e296e08979
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-08 22:50:50 +0300

    adding class Project

--- awwards/views.py
+++ awwards/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from django.db import models
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
 
+ Create your models here.
+
+class Project(models.Model):
+    title=models.CharField(max_length=100,null=False)
+    description=models.TextField(max_length=500)
+    screenshot1=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
+    screenshot2=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
+    screenshot3=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
+    link=models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
 
