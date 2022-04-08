46f2d1598da392a6e087d62e07f5798af478affe
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-08 23:05:50 +0300

    adding login function

--- awwards/views.py
+++ awwards/views.py
@@ -18,3 +18,25 @@ def registeruser(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            
+            
+        messages.success(request, 'Account Created Successfully!. Check out our Email later :)')
+
+            return redirect('login')
+    else:
+        form = CreateUserForm
+    context = {
+            'title':title,
+            'form':form,
+                        }
+    return render(request, 'registration/register.html', context)
+
+def loginpage(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
