9efd7454a1bfab220990917b31c733ab65fc4a65
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-08 22:52:18 +0300

    it will add class in models

--- awwards/views.py
+++ awwards/views.py
@@ -5,7 +5,7 @@ from django.db import models
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
 
- Create your models here.
+# Create your models here.
 
 class Project(models.Model):
     title=models.CharField(max_length=100,null=False)
@@ -25,4 +25,21 @@ class Project(models.Model):
 
     def delete_project(self):
         self.delete()
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    bio = TextField(max_length=500,null=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,blank=True)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
 
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+class Myprojects(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
