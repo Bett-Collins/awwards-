a3d937451f59b61f615f849e2e6df6fe607acef1
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-08 23:58:20 +0300

    adding test

---
+++ tests.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .models import Profile,Project
+
+# Create your tests here.
+#setup method
+class ProjectTestclass(TestCase):
+   
+    def setUp(self):
+        self.projects=Project(title='mytitle',description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
+
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.projects,Project))
+    #save method
+    def test_save_project(self):
+        self.projects.save_project()
+        projects=Project.objects.all()
+        self.assertTrue(len(projects)>0) 
