195f6c72bc068d3491dac8ded456dc1a6fe8d850
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-04-08 22:56:30 +0300

    adding views

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Project(models.Model):
+    title=models.CharField(max_length=100,null=False)
+    link=models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    bio = TextField(max_length=500,null=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,blank=True)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+class Myprojects(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+class Myprofile(models.Model):
+    name = models.CharField(max_length=40)
+    bio = models.TextField()
+    email = models.EmailField()
+    
--- awwards/views.py
+++ awwards/views.py
@@ -1,50 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from django.db import models
+
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def index(request):
+    projects=Project.objects.all()
 
-# Create your models here.
+    if 'item_name' in request.GET:
+        item_name=request.GET['item_name']
+        projects=projects.filter(title__icontains=item_name)
+    else:
+        projects=Project.objects.all()
 
-class Project(models.Model):
-    title=models.CharField(max_length=100,null=False)
-    description=models.TextField(max_length=500)
-    screenshot1=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
-    screenshot2=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
-    screenshot3=models.ImageField(default='default\.png',upload_to='screenshots/',blank=True)
-    link=models.CharField(max_length=100,blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
-
-
-    def __str__(self):
-        return self.title
-
-    def save_project(self):
-        self.save()
-
-    def delete_project(self):
-        self.delete()
-class Profile(models.Model):
-    avatar = models.ImageField(upload_to='avatars/')
-    bio = TextField(max_length=500,null=False)
-    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,blank=True)
-    name =models.CharField(max_length=100)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-class Myprojects(models.Model):
-    title = models.CharField(max_length=40)
-    description = models.TextField()
-class Myprofile(models.Model):
-    name = models.CharField(max_length=40)
-    bio = models.TextField()
-    email = models.EmailField()
-    
+    return render(request,'index.html',{'projects':projects})
